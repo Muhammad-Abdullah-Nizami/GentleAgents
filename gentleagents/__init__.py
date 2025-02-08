18ca4152e2f8753fe1937526b6445942081d30f0
Muhammad-Abdullah-Nizami <abdullah_nizami@live.com>
2025-02-08 22:35:17 +0500

    LICENSE, pyproject.toml, init file

---
+++ gentleagents/__init__.py
@@ -0,0 +1,5 @@
+from .loadenv import loadenv
+from gentleagents.agents.agent import Agent
+from gentleagents.teams.team import Team
+
+__all__ = ["Agent", "Team"]
--- gentleagents/loadenv/loadenv.py
+++ gentleagents/loadenv/loadenv.py
@@ -1,14 +1,14 @@
 from dotenv import load_dotenv
 import os
 
-PROJECT_ROOT_DIR = os.getcwd()
-BASE_DIR = f'{PROJECT_ROOT_DIR}'
-ENV_PATH = os.path.join(BASE_DIR, 'gentleagents/env/.env.development')
+DEFAULT_ENV_PATH = os.path.join(os.getcwd(), ".env")
+
+ENV_PATH = os.getenv("GENTLEAGENTS_ENV_PATH", DEFAULT_ENV_PATH)
+
 load_dotenv(ENV_PATH)
 
-
 OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
-SENDER_EMAIL=os.getenv('SENDER_EMAIL')
-SENDER_PASSWORD=os.getenv('SENDER_PASSWORD')
-GROQ_API_KEY=os.getenv('GROQ_API_KEY')
-GROQ_API_URL=os.getenv('GROQ_API_URL')
+SENDER_EMAIL = os.getenv('SENDER_EMAIL')
+SENDER_PASSWORD = os.getenv('SENDER_PASSWORD')
+GROQ_API_KEY = os.getenv('GROQ_API_KEY')
+GROQ_API_URL = os.getenv('GROQ_API_URL')
